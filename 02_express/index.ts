0b097db6f2804299d653da2575d562d31b482d60
NaotoShibui <apron.sketch@naotoseki.com>
2023-07-21 17:41:45 +0900

    edit uri

--- 02_express/index.ts
+++ 02_express/index.ts
@@ -4,13 +4,13 @@ import MaxAPI from 'max-api';
 const app = express();
 const port = 3000;
 
-app.post('/controll/1', (_req: Request, res: Response) => {
+app.post('/control/1', (_req: Request, res: Response) => {
     MaxAPI.outlet(1);
     res.status(204);
     res.send();
 });
 
-app.post('/controll/2', (_req: Request, res: Response) => {
+app.post('/control/2', (_req: Request, res: Response) => {
     MaxAPI.outlet(2);
     res.status(204);
     res.send();
